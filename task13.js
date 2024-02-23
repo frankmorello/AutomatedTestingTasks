108eca071ca0ceda2cea99e79c6f378060694b7e
frankmorello <obinnafokeke@gmail.com>
2024-02-23 07:44:48 +0100

    task13.js

---
+++ task13.js
@@ -0,0 +1,7 @@
+const books = {
+        title : 'Javascript / cypress Tutorials',
+        description : 'my new book',
+        numberOfPages : 8,
+        author : 'obinna',
+        reading : true
+       }
