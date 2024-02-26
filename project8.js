4bbe973c0476f2f183bd32c4cab855d35920266e
frankmorello <obinnafokeke@gmail.com>
2024-02-26 13:24:58 +0100

    project8.js

---
+++ project8.js
@@ -0,0 +1,10 @@
+function  booleanNum (num){
+ if(num % 10 ==0){
+  return true
+ }
+ else{
+    return false
+ }
+ }
+// pass value 1 inside the function
+ console.log(booleanNum(1)) // ruturn false
