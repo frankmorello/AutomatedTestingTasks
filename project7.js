098e67dee7184ebabaeb37faf3cb632ed2519e15
frankmorello <obinnafokeke@gmail.com>
2024-02-26 13:36:03 +0100

    project7.js

--- project7.js
+++ project7.js
@@ -1,3 +1,8 @@
 const numbersInDescendingOrder = [2, 5, 1, 12, 13, 3]
 // output numbers in descending order
-console.log(numbersInDescendingOrder.sort(function(a, b){return b-a}))
+const sort = numbersInDescendingOrder.sort(function(a, b)
+ {
+   return b-a
+
+})
+ console.log(sort);
