4ac216db908ac534ee2780ab55f4566558b5910f
frankmorello <obinnafokeke@gmail.com>
2024-02-26 13:16:21 +0100

    project4.js

---
+++ project4.js
@@ -0,0 +1,13 @@
+       // take input from the user
+        const input = parseInt(prompt('Enter a Number: '));
+
+      //creating a multiplication table
+   for(let i = 1; i <= 10; i++) {
+
+          // multiply i with number
+           const result = i * input;
+
+        // display the result 
+     console.log(input +'*' +i +'=' +result) 
+ } 
+ }
